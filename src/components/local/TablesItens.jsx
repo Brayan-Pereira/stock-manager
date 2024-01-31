e85654424e6a44fd2ca4f7dc803f5fb7462c5937
Brayan pereira <bp410794@gmail.com>
2024-01-31 17:56:55 -0300

    atualização pós teste navegadores

--- src/components/local/TablesItens.jsx
+++ src/components/local/TablesItens.jsx
@@ -18,47 +18,49 @@ export default function TableItens() {
     const storedItens = localStorage.getItem("lc-stck-mng")
     const itens = JSON.parse(storedItens)
 
-    if (itens.length === 0) {
-        return (<h2>Não itens no estoque</h2>)
-    } else {
-        return (
-            <div>
-                <br />
-                <table>
-                    <tr>
-                        <th className="id">ID</th>
-                        <th className="nome">Nome</th>
-                        <th className="estoque">Em estoque</th>
-                        <th className="categoria">Categoria</th>
-                        <th className="acoes">Ações</th>
-                    </tr>
-                    {
+    if (itens) {
+        if (itens.length === 0) {
+            return (<h2>Não itens no estoque</h2>)
+        } else {
+            return (
+                <div>
+                    <br />
+                    <table>
+                        <tr>
+                            <th className="id">ID</th>
+                            <th className="nome">Nome</th>
+                            <th className="estoque">Em estoque</th>
+                            <th className="categoria">Categoria</th>
+                            <th className="acoes">Ações</th>
+                        </tr>
+                        {
 
-                        itens.map((item) =>
-                        (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.name}</td>
-                                <td>{item.quant} unid.</td>
-                                <td>{item.category}</td>
-                                <td>
-                                    <Link to={`/itens/${item.id}`}>
-                                        <button className="btn_acoes_ver" >Ver</button>
-                                    </Link>
+                            itens.map((item) =>
+                            (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.quant} unid.</td>
+                                    <td>{item.category}</td>
+                                    <td>
+                                        <Link to={`/itens/${item.id}`}>
+                                            <button className="btn_acoes_ver" >Ver</button>
+                                        </Link>
 
-                                    <Link to={`/itens/update/${item.id}`}>
-                                        <button className="btn_acoes_atualizar">Atualizar</button>
-                                    </Link>
-                                    <button className="btn_acoes_excluir" onClick={() => Remove(item.id)}>Excluir</button></td>
-                            </tr>
-                        )
+                                        <Link to={`/itens/update/${item.id}`}>
+                                            <button className="btn_acoes_atualizar">Atualizar</button>
+                                        </Link>
+                                        <button className="btn_acoes_excluir" onClick={() => Remove(item.id)}>Excluir</button></td>
+                                </tr>
+                            )
 
-                        )}
-                </table>
-            </div >
+                            )}
+                    </table>
+                </div >
 
 
-        )
+            )
+        }
     }
 
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,51 +6,59 @@ export default function Home() {
 
     const storage = localStorage.getItem("lc-stck-mng")
 
-    const itens = JSON.parse(storage)
-
     const diversityItens = () => {
+        const itens = JSON.parse(storage)
         let count = 0
         let categories = ""
-        itens.map((item) => {
-            if (item.category !== categories) {
-                count++
-                categories = item.category
-            }
-        })
+
+        if (itens) {
+            itens.map((item) => {
+                if (item.category !== categories) {
+                    count++
+                    categories = item.category
+                }
+            })
+        }
         return count
     }
 
     const totalInventory = () => {
+        const itens = JSON.parse(storage)
         let quant = 0
-        itens.map(item => quant += parseInt(item.quant))
+        if (itens) {
+            itens.map(item => quant += parseInt(item.quant))
+        }
         return quant
     }
 
     const scarce = () => {
+        const itens = JSON.parse(storage)
         let quant = 0
 
         let arrayScarce = []
+        if (itens) {
+            itens.map((item) => {
+                if (item.quant <= 10) {
+                    quant++
 
-        itens.map((item) => {
-            if (item.quant <= 10) {
-                quant++
+                    let scarceItem = {
+                        name: item.name,
+                        id: item.id
+                    }
 
-                let scarceItem = {
-                    name: item.name,
-                    id: item.id
+                    arrayScarce.push(scarceItem)
+
                 }
-
-                arrayScarce.push(scarceItem)
-
-            }
-            else { quant = quant }
-        })
+                else { quant = quant }
+            })
+        }
         return { quant, arrayScarce }
     }
     const scarceItems = scarce()
 
 
     const recent = () => {
+        const itens = JSON.parse(storage)
         let quant = 0
         const dt = new Date()
         const currentDate = {
@@ -61,29 +69,29 @@ export default function Home() {
 
         let arrayRecent = []
 
-        itens.map((item) => {
+        if (itens) {
+            itens.map((item) => {
 
-            if ((+currentDate.year - +item.dt.year) <= 1) {
-                if (+currentDate.month - +item.dt.monthIndice <= 1 || +currentDate.month - +item.dt.monthIndice === -11) {
-                    if (+currentDate.day - item.dt.day <= 5 || +currentDate.day - item.dt.day <= -25) {
-                        let recentItem = {
-                            name: item.name,
-                            id: item.id
+                if ((+currentDate.year - +item.dt.year) <= 1) {
+                    if (+currentDate.month - +item.dt.monthIndice <= 1 || +currentDate.month - +item.dt.monthIndice === -11) {
+                        if (+currentDate.day - item.dt.day <= 5 || +currentDate.day - item.dt.day <= -25) {
+                            let recentItem = {
+                                name: item.name,
+                                id: item.id
+                            }
+                            arrayRecent.push(recentItem)
+                            quant++
+
                         }
-                        arrayRecent.push(recentItem)
-                        quant++
-
                     }
                 }
-            }
-        })
+            })
+        }
 
-        return {quant, arrayRecent}
+        return { quant, arrayRecent }
     }
 
     const recentItens = recent()
-
-    console.log(itens)
 
     return (
         <div>
@@ -92,12 +100,12 @@ export default function Home() {
             <section className="cards">
                 <QuantityCard description="Diversidade de itens" quant={diversityItens ? diversityItens() : 0} />
                 <QuantityCard description="Inventário total" quant={totalInventory ? totalInventory() : 0} />
-                <QuantityCard description="Itens recentes" quant={recentItens? recentItens.quant : 0} />
+                <QuantityCard description="Itens recentes" quant={recentItens ? recentItens.quant : 0} />
                 <QuantityCard description="Itens acabando" quant={scarceItems ? scarceItems.quant : 0} />
             </section>
 
             <section className="Tables">
-                <Table status="Recent Itens" obj={recentItens.arrayRecent}/>
+                <Table status="Recent Itens" obj={recentItens.arrayRecent} />
                 <Table status="Scarce Itens" obj={scarceItems.arrayScarce} />
             </section>
         </div>
